55c7ec96ce183d4b12295d1bc661bf5c20772555
vinu1A23 <tan2002vinu@gmail.com>
2024-02-06 20:30:51 +0530

    adding comments to find angle

--- PoseModule_2.py
+++ PoseModule_2.py
@@ -9,9 +9,11 @@ import cv2 # Import OpenCV library for computer vision
 import mediapipe as mp # Import Mediapipe library for pose estimation
 import time # Import time library to calculate FPS
 import math # Import math library for mathematical functions
+
+# Defining a class called poseDetector
 class poseDetector():
     def __init__(self, mode=False, upBody=False, smooth=True,
-                 detectionCon=True, trackCon=True,debugger=False):  # Defining a class called poseDetector
+                 detectionCon=True, trackCon=True,debugger=False):  
         
         #mode - Boolean, uses RGB camera feed if True, BGR otherwise 
         #upBody - Boolean, tracks full body if False, upper body only if True
@@ -70,34 +72,52 @@ class poseDetector():
                 #w- weight
                 
                 h, w, c= img.shape
+
+                #cx - x-coordinate
+                #cy - y-coordinate
                 
                 cx,cy=int(lm.x*w), int(lm.y*h)
+                
                 self.lmList.append([id,cx,cy])
+                
                 if draw or self.debugger:
-                    cv2.circle (img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                    cv2.circle (img, (cx, cy), 5, (255, 0, 0), cv2.FILLED) #draw circle
+
+        #return list of landmarks
         return self.lmList
-    
+
+    #Function findAngle used to find angle between 3 points
     def findAngle(self,img, p1, p2, p3, draw=False) :
+
+        #img - Numpy array of image data
+        #p1 - first landmark point
+        #p2 - second/mid landmark point
+        #p1 - third landmark point
+        
         #get the landmark
         x1, y1 = self. lmList [p1] [1:]
         x2, y2 = self. lmList [p2] [1 : ]
         x3, y3 = self. lmList [p3] [1:]
+        
         #calculate the angle
         angle= math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2 (y1 - y2, x1 - x2))
+
+        #if angle is negative then angle+360        
         if angle<0:
             angle+=360
         
         if draw or self.debugger:
-            cv2.line (img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.line (img, (x3, y3), (x2, y2), (255, 255, 255), 3)
-            cv2.circle (img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle (img, (x1, y1), 15, (0, 0, 255), 2)
-            cv2.circle (img, (x2, y2), 10, (0, 0, 255), cv2. FILLED)
-            cv2.circle (img, (x2, y2), 15, (0, 0, 255), 2)
-            cv2.circle (img, (x3, y3), 10, (0, 0, 255), cv2. FILLED)
-            cv2.circle (img, (x3, y3), 15, (0, 0, 255), 2)
+            
+            cv2.line (img, (x1, y1), (x2, y2), (255, 255, 255), 3)   #line between 1st and 2nd point
+            cv2.line (img, (x3, y3), (x2, y2), (255, 255, 255), 3)   #line between 3rd and 2nd point
+            cv2.circle (img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)  #filled circle on 1st point
+            cv2.circle (img, (x1, y1), 15, (0, 0, 255), 2)           #unfilled circle on 1st circle
+            cv2.circle (img, (x2, y2), 10, (0, 0, 255), cv2. FILLED) #filled circle on 2nd point
+            cv2.circle (img, (x2, y2), 15, (0, 0, 255), 2)           #unfilled circle on 2nd point
+            cv2.circle (img, (x3, y3), 10, (0, 0, 255), cv2. FILLED) #filled circle on 3rd point
+            cv2.circle (img, (x3, y3), 15, (0, 0, 255), 2)           #unfilled circle on 3rd point
             cv2.putText (img , str(int (angle) ) , (x2 - 50, y2-50),
-                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)  #print angle on screen
         return angle
 
     def findAngle2(self, img, p1, p2, p3,draw=False):
