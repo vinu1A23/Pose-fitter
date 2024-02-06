adaeaeabe791ac4001e59b5e3dcc781a316b6877
vinu1A23 <tan2002vinu@gmail.com>
2024-02-06 18:51:16 +0530

    updating with some comments

--- PoseModule_2.py
+++ PoseModule_2.py
@@ -5,31 +5,31 @@ Created on Mon Mar 14 20:23:51 2022
 @author: dell
 """
 
-import cv2
-import mediapipe as mp
-import time
-import math
+import cv2 # Import OpenCV library for computer vision
+import mediapipe as mp # Import Mediapipe library for pose estimation
+import time # Import time library to calculate FPS
+import math # Import math library for mathematical functions
 class poseDetector():
     def __init__(self, mode=False, upBody=False, smooth=True,
-                 detectionCon=True, trackCon=True,debugger=False):  ##detectionCon ,, trackCon
-        self.mode=mode
-        self.upBody=upBody
-        self.smooth= smooth
-        self. detectionCon = detectionCon
-        self.trackCon = trackCon
-        self.mpDraw = mp. solutions .drawing_utils
-        self . mpPose = mp.solutions .pose
-        self . pose = self.mpPose . Pose (self. mode, self. upBody, self. smooth,self. detectionCon, self. trackCon)
-        self.debugger=debugger
+                 detectionCon=True, trackCon=True,debugger=False):  # Defining a class called poseDetector
+        self.mode=mode # Boolean, uses RGB camera feed if True, BGR otherwise 
+        self.upBody=upBody # Boolean, tracks full body if False, upper body only if True
+        self.smooth= smooth # Boolean, smoothens/filters positions in frames if True
+        self. detectionCon = detectionCon # Confidence value for detection
+        self.trackCon = trackCon # Confidence value for tracking
+        self.mpDraw = mp. solutions .drawing_utils # Mediapipe drawing utilities 
+        self . mpPose = mp.solutions .pose # Mediapipe pose estimation model
+        self . pose = self.mpPose . Pose (self. mode, self. upBody, self. smooth,self. detectionCon, self. trackCon)# Initializes the pose model object
+        self.debugger=debugger # Boolean, shows debug info if True
     def findPose (self, img, draw=False) :
         
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self. results = self. pose . process (imgRGB)
-        if self.results . pose_landmarks:
-            if draw or self.debugger:
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Convert the image from BGR to RGB color format for processing
+        self. results = self. pose . process (imgRGB) #Run the image through the pose estimation model
+        if self.results . pose_landmarks: #If pose landmarks were detected
+            if draw or self.debugger: #If set to draw the landmarks or debugger is enabled
                 self.mpDraw. draw_landmarks (img, self. results .pose_landmarks,self. mpPose . POSE_CONNECTIONS)
         return img , self.results.pose_landmarks
-    def findPosition (self, img, draw=False) :
+    def findPosition (self, img, draw=False) :#Define the findPosition method with parameters for the image and an option to draw
         self.lmList=[]
         if self.results.pose_landmarks :
             for id, lm in enumerate (self. results . pose_landmarks. landmark):
